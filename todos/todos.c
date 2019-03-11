0f552796c6a84e51c63413fdd065ed4f3c07d8ae
¨Rene Richarz <rene.richarz@bluewin.ch>
2019-03-11 10:04:00 +0100

    fixed tab with left margin

--- todos/todos.c
+++ todos/todos.c
@@ -2,24 +2,34 @@
 /*  rricharz 2019 for 2.11BSD Unix on PiDP-11	*/
 /*  Uses Berkley C syntax!			*/
 /*  Converts lf to crlf				*/
+/*  Adds a left margin for puncher and folder	*/
+/*  Converts tabs to spaces			*/
 
 #include <stdio.h>
 #include <stdlib.h>
 
-#define LEFTMARGIN	4
+#define LEFTMARGIN	6
 
 int main(argc, argv)
 int argc;
 char *argv[];
 {
-	int c,i;
+	int c,i,pos;
 	for (i=0; i<LEFTMARGIN; i++) putchar(' ');
+	pos=0;	/* tab expansion after left margin */
 	while ((c=getchar())!=EOF) {
-		putchar(c);
 		if (c=='\n') {
 			putchar('\r');
-			for (i=0; i<LEFTMARGIN; i++) putchar(' ');
+			pos=0;
 		}
+		if (c=='\t')
+			do putchar(' ');
+			while (pos++ % 8);
+		else {
+			putchar(c); pos++;
+		}
+		if (c=='\n')
+			for (i=0; i<LEFTMARGIN; i++) putchar(' ');
 	}
 	exit(0);
 }
